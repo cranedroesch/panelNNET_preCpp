95c388adc2f9bc352d101f4d673a677c1882aabe
cranedroesch <andrewcd@gmail.com>
2016-12-13 10:19:49 -0500

    Fixed sandwich vcv estimator -- sum(e[i]^2*J[i,] %*% t(J[i,])), not t(J)%*%e^2%*%t(e^2)%*%J

--- R/vcov.panelNNET.R
+++ R/vcov.panelNNET.R
@@ -1,12 +1,8 @@
 vcov.panelNNET <-
 function(obj, option, J = NULL){
-#obj = pnn
-#option = 'Jacobian_sandwich'
-#J = NULL
   e <- obj$y - obj$yhat
   if (is.null(J) & grepl('Jacobian', option)){  J <- Jacobian.panelNNET(obj)
     } else {J <- obj$hidden_layers[[length(obj$hidden_layers)]]}
-
   D <- rep(obj$lam, ncol(J))
   if (is.null(obj$fe_var)){
     pp <- c(1, obj$parapen) #parapen
@@ -15,12 +11,14 @@ function(obj, option, J = NULL){
   }
   D[1:length(pp)] <- D[1:length(pp)]*pp #incorporate parapen into diagonal of covmat
   bread <- solve(t(J) %*% J + diag(D))
-
   if (option == 'Jacobian_homoskedastic'){
     vcov <- sum(e^2)/(length(e) - ncol(J)) * bread
   }
   if (option == 'Jacobian_sandwich'){
-    vcov <- (length(e)-1)/(length(e) - ncol(J)) * bread %*% t(J) %*% e^2 %*% t(e^2) %*% J %*% bread
+    meat <- foreach(i = 1:length(e), .combine = '+') %do% {
+      e[i]^2*J[i,] %*% t(J[i,])
+    }
+    vcov <- (length(e)-1)/(length(e) - ncol(J)) * bread %*% meat %*% bread
   }
   if (option == 'Jacobian_cluster'){
     G <- length(unique(obj$fe_var))
@@ -35,7 +33,10 @@ function(obj, option, J = NULL){
     vcov <- sum(e^2)/(length(e) - ncol(J)) * bread
   }
   if (option == 'sandwich'){
-    vcov <- (length(e)-1)/(length(e) - ncol(J)) * bread %*% t(J) %*% e^2 %*% t(e^2) %*% J %*% bread
+    meat <- foreach(i = 1:length(e), .combine = '+') %do% {
+      e[i]^2*J[i,] %*% t(J[i,])
+    }
+    vcov <- (length(e)-1)/(length(e) - ncol(J)) * bread %*% meat %*% bread
   }
   if (option == 'cluster'){
     G <- length(unique(obj$fe_var))
