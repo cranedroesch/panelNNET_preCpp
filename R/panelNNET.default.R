b7687ad883f533ff7abb23bd0d5c5cda6983325b
cranedroesch <andrewcd@gmail.com>
2016-12-13 10:01:58 -0500

    Changed default on parapen to zeros -- parametric terms unpenalized by default

--- R/panelNNET.default.R
+++ R/panelNNET.default.R
@@ -1,7 +1,7 @@
 panelNNET.default <-
 function(y, X, hidden_units, fe_var, seed = 1
   , maxit = 1000, lam = 0, time_var = NULL, param = NULL
-  , parapen = rep(1, ncol(param)), parlist = NULL, verbose = FALSE
+  , parapen = rep(0, ncol(param)), parlist = NULL, verbose = FALSE
   , save_each_iter = FALSE, path = NULL, tag = "", gravity = 1.01
   , convtol = 1e-8, bias_hlayers = FALSE, RMSprop = FALSE, start_LR = .01
   , activation = 'tanh', inference = TRUE, doscale = TRUE, ...)
