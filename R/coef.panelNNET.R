b24e7d2af4b0bf641adb46a6d816ff6623684b58
cranedroesch <andrewcd@gmail.com>
2016-12-15 11:51:37 -0500

    added coef method

---
+++ R/coef.panelNNET.R
@@ -0,0 +1,23 @@
+coef.panelNNET <- function(obj, rescale = TRUE){
+  parm <- obj$parlist$beta_param
+  if (rescale == TRUE & obj$doscale == TRUE){
+    parm <- parm/attr(obj$param, "scaled:scale")
+  }
+  if (!is.null(m$vcs)){
+    se <- lapply(obj$vcs, function(x){
+      se <- sqrt(diag(x$vc))[1:length(parm)]
+      if (rescale == TRUE & obj$doscale == TRUE){
+        se <- se/attr(obj$param, "scaled:scale")
+      }
+      return(se)
+    })
+    rn <- names(se)
+    se <- t(matrix(unlist(se), length(se[[1]])))
+    colnames(se) <- colnames(m$param)
+    rownames(se) <- rn
+  } else {
+    se <- 'No parameter covariance matrix in fitted model!'
+  }
+  return(list(parm = parm, se = se))
+}
+
