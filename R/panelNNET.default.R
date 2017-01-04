9d4f540668de1a0c69e99a98467ec5174add456c
cranedroesch <andrewcd@gmail.com>
2017-01-04 15:10:15 -0500

    batchsize added to default method

--- R/panelNNET.default.R
+++ R/panelNNET.default.R
@@ -5,13 +5,13 @@ function(y, X, hidden_units, fe_var
   , save_each_iter = FALSE, path = NULL, tag = "", gravity = 1.01
   , convtol = 1e-8, bias_hlayers = FALSE, RMSprop = FALSE, start_LR = .01
   , activation = 'tanh', inference = TRUE, doscale = TRUE
-  , treatment = NULL, interact_treatment = TRUE, ...)
+  , treatment = NULL, interact_treatment = TRUE, batchsize = nrow(X), ...)
 {
   out <- panelNNET.est(y, X, hidden_units, fe_var, maxit, lam
     , time_var, param, parapen, parlist, verbose, save_each_iter
     , path, tag, gravity, convtol, bias_hlayers, RMSprop
     , start_LR, activation, inference, doscale 
-    , treatment, interact_treatment, ...
+    , treatment, interact_treatment, batchsize, ...
   )
   out$call = match.call()
   class(out) <- 'panelNNET'
