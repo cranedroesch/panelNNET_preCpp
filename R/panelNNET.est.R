013608528369cf5bbbbb16f0cd3c459476c06205
cranedroesch <andrewcd@gmail.com>
2017-01-06 13:26:26 -0500

    loss recorded redundantly in output; removed

--- R/panelNNET.est.R
+++ R/panelNNET.est.R
@@ -325,7 +325,7 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
   output <- list(yhat = yhat, parlist = parlist, hidden_layers = hlayers
     , fe = fe_output, converged = conv, mse = mse, loss = loss, lam = lam, time_var = time_var
     , X = X, y = y, param = param, fe_var = fe_var, hidden_units = hidden_units, maxit = maxit
-    , used_bias = bias_hlayers, final_improvement = D, msevec = msevec, loss = loss, RMSprop = RMSprop, convtol = convtol
+    , used_bias = bias_hlayers, final_improvement = D, msevec = msevec, RMSprop = RMSprop, convtol = convtol
     , grads = grads, activation = activation, parapen = parapen, doscale = doscale, treatment = treatment
     , interact_treatment = interact_treatment, batchsize = batchsize
   )
