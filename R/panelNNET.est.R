0c9d40dadc9738b9dfc25f5fb9b1a0a139500506
cranedroesch <andrewcd@gmail.com>
2017-01-13 13:04:57 -0500

    First attempt at GPU implementation.  Initial tests show no speedup.  Possible explainations include communication time

--- R/panelNNET.est.R
+++ R/panelNNET.est.R
@@ -1,34 +1,55 @@
-panelNNET.est <-
-function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parlist, verbose, save_each_iter, path, tag, gravity, convtol, bias_hlayers, RMSprop, start.LR, activation, inference, doscale, treatment, interact_treatment, batchsize, maxstopcounter, OLStrick, ...){
+#panelNNET.est <-
+#function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parlist, verbose, save_each_iter, path, tag, gravity, convtol, bias_hlayers, RMSprop, start.LR, activation, inference, doscale, treatment, interact_treatment, batchsize, maxstopcounter, OLStrick, ...){
 ##examplearguments for testing
+#library(gmatrix)
+library(devtools)
+install_github('cranedroesch/panelNNET', ref = 'gpu')
+library(panelNNET)
+N <- 10000
+x <- sort(runif(N, 0, 20))
+u <- rnorm(N, sd = 5)
+id <- (1:N-1)%/%100+1
+time <- 1:N/100
+y <- id + time + 3*x*sin(x*3) + u
+#plot(x, y)
+
 #y = y[r]
-#X = Z[r,]
-#hidden_units = c(100, 50)
-#fe_var = id[r]
-#maxit = 1000
-#lam = 1
+X = matrix(x)
+hidden_units = c(300, 300, 300)
+fe_var = factor(id)
+maxit = 1000
+lam = 1
 
-#time_var = time[r]
-#param = P[r,]
-#parlist = NULL
-#verbose = TRUE
-#OLStrick = TRUE
-#save_each_iter = FALSE
-#path = NULL
-#tag = ""
-#gravity = 1.01
-#bias_hlayers = TRUE
-# RMSprop = TRUE
-#convtol = 1e-8
-#activation = 'tanh'
-#doscale = TRUE
-#inference = FALSE
-#batchsize = nrow(X)
-#parapen = rep(1, ncol(P))
-#treatment = NULL
-#start.LR = .01
-#maxstopcounter = 10
+time_var = matrix(time)
+param = matrix(time)
+parlist = NULL
+verbose = FALSE
+OLStrick = TRUE
+save_each_iter = FALSE
+path = NULL
+tag = ""
+gravity = 1.01
+bias_hlayers = TRUE
+ RMSprop = TRUE
+convtol = 1e-8
+activation = 'tanh'
+doscale = TRUE
+inference = FALSE
+batchsize = nrow(X)
+parapen = rep(1, ncol(param))
+treatment = NULL
+start.LR = .01
+maxstopcounter = 10
 
+
+GPU = FALSE
+
+if (GPU == TRUE){
+  g <- gmatrix::g
+  h <- gmatrix::h
+} else {
+  g <-h <- function(x){x}
+}
   if (doscale == TRUE){
     X <- scale(X)
     if (!is.null(param)){
@@ -63,7 +84,7 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
     parlist <- vector('list', nlayers)
     for (i in 1:nlayers){
       if (i == 1){D <- ncol(X)} else {D <- hidden_units[i-1]}
-      parlist[[i]] <- matrix(runif((hidden_units[i])*(D+bias_hlayers), -.7, .7), ncol = hidden_units[i])
+      parlist[[i]] <- g(matrix(runif((hidden_units[i])*(D+bias_hlayers), -.7, .7), ncol = hidden_units[i]))
     }
     parlist$beta <- runif(hidden_units[i], -.7, .7)
     if (is.null(param)){
@@ -76,7 +97,7 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
       warning('WARNING: panelNNET for heterogeneous treatment effects is still highly experimental.  the gradient descent algorithm appears to suffer badly from local minima and the standard errors of the treatment effects appear to be extremely buggy.')
       parlist$beta_treatment <- runif(1, -.7, .7)
       if (interact_treatment == TRUE){
-        parlist$beta_treatmentinteractions <- runif(hidden_units[i], -.7, .7)
+        parlist$beta_treatmentinteractions <- g(runif(hidden_units[i], -.7, .7))
       }
     }
     #add the bias term/intercept onto the front, if there are no FE's
@@ -89,12 +110,13 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
   #calculate hidden layers
   hlayers <- vector('list', nlayers)
   for (i in 1:nlayers){
-    if (i == 1){D <- X} else {D <- hlayers[[i-1]]}
-    if (bias_hlayers == TRUE){D <- cbind(1, D)}
+    if (i == 1){D <- g(X)} else {D <- hlayers[[i-1]]}
+    if (bias_hlayers == TRUE){D <- g(cbind(1, h(D)))}
     hlayers[[i]] <- sigma(D %*% parlist[[i]])
   }
   colnames(hlayers[[i]]) <- paste0('nodes',1:ncol(hlayers[[i]]))
   if (!is.null(treatment)){
+    if(GPU == TRUE){stop('GPU not yet implemented for HTE models')}
     #Add treatment interactions
     if (interact_treatment == TRUE){
       ints <- sweep(hlayers[[i]], 1, treatment, '*')
@@ -106,32 +128,40 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
     colnames(hlayers[[i]])[1] <- 'treatment'
   }
   if (!is.null(param)){
-    hlayers[[i]] <- cbind(param, hlayers[[i]])
+    hlayers[[i]] <- g(cbind(param, h(hlayers[[i]])))
     colnames(hlayers[[i]])[1:ncol(param)] <- paste0('param',1:ncol(param))
   }
-  if (is.null(fe_var)){hlayers[[i]] <- cbind(1, hlayers[[i]])}#add intercept if no FEs
+  if (is.null(fe_var)){hlayers[[i]] <- g(cbind(1, h(hlayers[[i]])))}#add intercept if no FEs
   #get starting MSE
   if (!is.null(fe_var)){
-    Zdm <- demeanlist(hlayers[[i]], list(fe_var))
+    Zdm <- g(demeanlist(h(hlayers[[i]]), list(fe_var)))
     ydm <<- demeanlist(y, list(fe_var))
-    fe <- (y-ydm) - as.matrix(hlayers[[i]]-Zdm) %*% as.matrix( c(parlist$beta_param, parlist$beta_treatment, parlist$beta_treatmentinteractions, parlist$beta))
-    yhat <- hlayers[[i]] %*% c(parlist$beta_param, parlist$beta_treatment, parlist$beta_treatmentinteractions, parlist$beta) + fe    
+    topLevelPars <- g(as.matrix( c(parlist$beta_param, parlist$beta_treatment, parlist$beta_treatmentinteractions, parlist$beta)))
+    fe <- (y-ydm) - (hlayers[[i]]-Zdm) %*% topLevelPars
+    yhat <- hlayers[[i]] %*% topLevelPars
   } else {
-    yhat <- hlayers[[i]] %*% c(parlist$beta_param, parlist$beta_treatment, parlist$beta_treatmentinteractions, parlist$beta)
+    yhat <- hlayers[[i]] %*% topLevelPars
   }
-  mse <- mseold <- mean((y-yhat)^2)
-  loss <- lossold <- mse + lam*sum(c(parlist$beta_param*parapen, 0*parlist$beta_treatment, parlist$beta, parlist$beta_treatmentinteractions, unlist(parlist[!grepl('beta', names(parlist))]))^2)
+  mse <- mseold <- mean((y-h(yhat))^2)
+
+if (GPU == TRUE){
+  lowParVec <- unlist(sapply(parlist[sapply(parlist, class) == 'gmatrix'], h))
+} else {
+  lowParVec <- unlist(parlist[!grepl('beta', names(parlist))])
+}
+  lossold <- Inf
+  loss <- mse + lam*sum(c(parlist$beta_param*parapen, 0*parlist$beta_treatment, parlist$beta, parlist$beta_treatmentinteractions, lowParVec)^2)
   #Calculate gradients.  These aren't the actual gradients, but become the gradients when multiplied by their respective layer.
   grads <- vector('list', nlayers+1)
   grads[[length(grads)]] <- getDelta(y, yhat)
   for (i in (nlayers):1){
-    if (i == nlayers){outer_param = as.matrix(c(parlist$beta))} else {outer_param = parlist[[i+1]]}
-    if (i == 1){lay = X} else {lay= hlayers[[i-1]]}
+    if (i == nlayers){outer_param = g(as.matrix(c(parlist$beta)))} else {outer_param = parlist[[i+1]]}
+    if (i == 1){lay = g(X)} else {lay= hlayers[[i-1]]}
     if (bias_hlayers == TRUE){
-      lay <- cbind(1, lay) #add bias to the hidden layer
-      if (i!=nlayers){outer_param <- outer_param[-1,]}      #remove parameter on upper-layer bias term
+      lay <- g(cbind(1, h(lay))) #add bias to the hidden layer
+      if (i!=nlayers){outer_param <- g(h(outer_param)[-1,])}      #remove parameter on upper-layer bias term
     }
-    grads[[i]] <- getS(D_layer = lay, inner_param = parlist[[i]], outer_deriv = grads[[i+1]], outer_param = outer_param, activation)
+    grads[[i]] <- getS(D_layer = lay, inner_param = parlist[[i]], outer_deriv = grads[[i+1]], outer_param = h(outer_param), activation)
   }
   #Initialize updates
   updates <- lapply(parlist, function(x){x*0})
@@ -155,28 +185,30 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
       , yhat = yhat, mse = mse, mseold = mseold, loss = loss, lossold = lossold, updates = updates, G2 = G2)
     #Start epoch
     #Assign batches
+PT <- proc.time()
     batchid <- sample(1:nrow(X)%/%batchsize +1)
     if (min(table(batchid))<(batchsize/2)){#Deal with orphan batches
       batchid[batchid == max(batchid)] <- sample(1:(max(batchid) - 1), min(table(batchid)), replace = TRUE)
     }
     for (bat in 1:max(batchid)) {
+#bat <- 1
       curBat <- which(batchid == bat)
       #Get updated gradients
       grads <- vector('list', nlayers+1)
-      grads[[length(grads)]] <- getDelta(y[curBat], yhat[curBat])
+      grads[[length(grads)]] <- g(matrix(getDelta(y[curBat], h(yhat)[curBat])))
       for (i in (nlayers):1){
-        if (i == nlayers){outer_param = as.matrix(c(parlist$beta))} else {outer_param = parlist[[i+1]]}
-        if (i == 1){lay = X[curBat,]} else {lay= hlayers[[i-1]][curBat,]}
+        if (i == nlayers){outer_param = g(as.matrix(c(parlist$beta)))} else {outer_param = parlist[[i+1]]}
+        if (i == 1){lay = g(as.matrix(X[curBat,]))} else {lay= hlayers[[i-1]][curBat,]}
         if (bias_hlayers == TRUE){
-          lay <- cbind(1, lay) #add bias to the hidden layer
-          if (i!=nlayers){outer_param <- outer_param[-1,]}      #remove parameter on upper-layer bias term
+          lay <- g(cbind(1, h(lay))) #add bias to the hidden layer
+          if (i!=nlayers){outer_param <- g(h(outer_param)[-1,])}     #remove parameter on upper-layer bias term
         }
-        grads[[i]] <- getS(D_layer = lay, inner_param = parlist[[i]], outer_deriv = grads[[i+1]], outer_param = outer_param, activation)
+        grads[[i]] <- getS(D_layer = lay, inner_param = parlist[[i]], outer_deriv = grads[[i+1]], outer_param = h(outer_param), activation)
       }
       #Calculate updates to parameters based on gradients and learning rates
       if (RMSprop == TRUE){
         newG2 <- foreach(i = 1:(length(hlayers)+1)) %do% {
-          if (i == 1){D <- X[curBat,]} else {D <- hlayers[[i-1]][curBat,]}
+          if (i == 1){D <- X[curBat,]} else {D <- h(hlayers[[i-1]])[curBat,]}
           if (bias_hlayers == TRUE & i != length(hlayers)+1){D <- cbind(1, D)}
             .1*(t(D) %*% grads[[i]])^2
         }
@@ -185,8 +217,8 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
         uB <- LR/sqrt(G2[[length(G2)]]+1e-10) *
           t(t(grads[[length(grads)]]) %*% hlayers[[length(hlayers)]][curBat,]) + 
           LR*as.matrix(2*lam*c(parlist$beta_param*parapen, 0*parlist$beta_treatment, parlist$beta, parlist$beta_treatmentinteractions))#Treatment is always unpenalized
-        updates$beta_param <- uB[1:length(parlist$beta_param)]
-        updates$beta <- uB[grepl('nodes', rownames(uB))]
+        updates$beta_param <- h(uB)[1:length(parlist$beta_param)]
+        updates$beta <- h(uB)[grepl('nodes', rownames(uB))]
         if (!is.null(treatment)){
           updates$beta_treatment <- uB[rownames(uB) == 'treatment']
           if (interact_treatment == TRUE){
@@ -194,11 +226,12 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
           }
         }
         for(i in nlayers:1){
-          if(i == 1){lay = X[curBat,]} else {lay = hlayers[[i-1]][curBat,]}
-          if(bias_hlayers == TRUE){lay <- cbind(1,lay)}
+          if(i == 1){lay = g(matrix(X[curBat,]))} else {lay = g(h(hlayers[[i-1]])[curBat,])}
+          if(bias_hlayers == TRUE){lay <- g(cbind(1,h(lay)))}
           updates[[i]] <- LR/sqrt(G2[[i]]+1e-10) * t(t(grads[[i]]) %*% lay) + LR*t(2 * lam * t(parlist[[i]]))
         }
       } else { #if RMSprop == FALSE
+        if (GPU == TRUE) {'non-RMSprop not implemented for GPU'}
         uB <- LR * t(t(grads[[length(grads)]]) %*% hlayers[[length(hlayers)]][curBat,] +
           2*lam*c(parlist$beta_param*parapen, 0*parlist$beta_treatment, parlist$beta, parlist$beta_treatmentinteractions))
         updates$beta_param <- uB[1:length(parlist$beta_param)]
@@ -219,8 +252,8 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
       parlist <- mapply('-', parlist, updates)
       #Update hidden layers
       for (i in 1:nlayers){
-        if (i == 1){D <- X} else {D <- hlayers[[i-1]]}
-        if (bias_hlayers == TRUE){D <- cbind(1, D)}
+        if (i == 1){D <- g(X)} else {D <- hlayers[[i-1]]}
+        if (bias_hlayers == TRUE){D <- g(cbind(1, h(D)))}
         hlayers[[i]] <- sigma(D %*% parlist[[i]])
       }
       colnames(hlayers[[i]]) <- paste0('nodes',1:ncol(hlayers[[i]]))
@@ -236,13 +269,13 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
         colnames(hlayers[[i]])[1] <- 'treatment'
       }
       if (!is.null(param)){
-        hlayers[[i]] <- cbind(param, hlayers[[i]])
+        hlayers[[i]] <- g(cbind(param, h(hlayers[[i]])))
         colnames(hlayers[[i]])[1:ncol(param)] <- paste0('param',1:ncol(param))
       }
-      if (is.null(fe_var)){hlayers[[i]] <- cbind(1, hlayers[[i]])}#add intercept if no FEs
+      if (is.null(fe_var)){hlayers[[i]] <- g(cbind(1, h(hlayers[[i]])))}#add intercept if no FEs
       #update yhat
       if (!is.null(fe_var)){
-        Zdm <- demeanlist(hlayers[[i]], list(fe_var))
+        Zdm <- g(demeanlist(h(hlayers[[i]]), list(fe_var)))
         if (OLStrick == TRUE){#OLS trick!
           lamvec <- rep(lam, ncol(Zdm))
           if (is.null(fe_var)){
@@ -252,27 +285,33 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
           }
           lamvec[1:length(pp)] <- lamvec[1:length(pp)]*pp #incorporate parapen into diagonal of covmat
 
-          B <- solve(t(Zdm) %*% Zdm + diag(lamvec)) %*% t(Zdm) %*% ydm
-          parlist$beta <- B[grepl('nodes', rownames(B))]
-          parlist$beta_param <- B[grepl('param', rownames(B))]
+          B <- solve(t(Zdm) %*% Zdm + g(diag(lamvec))) %*% t(Zdm) %*% ydm
+          parlist$beta <- h(B)[grepl('nodes', colnames(Zdm))]
+          parlist$beta_param <- h(B)[grepl('param', colnames(Zdm))]
           if (!is.null(treatment)){
             parlist$beta_treatment <- B[grepl('treatment', rownames(B))]
             parlist$beta_treatmentinteractions <- B[grepl('TrInts', rownames(B))]
           }
         }
-        fe <- (y-ydm) - as.matrix(hlayers[[i]]-Zdm) %*% as.matrix(c(
+        fe <- (y-ydm) - (hlayers[[i]]-Zdm) %*% g(as.matrix(c(
             parlist$beta_param, parlist$beta_treatment
           , parlist$beta_treatmentinteractions, parlist$beta
-        ))
-        yhat <- hlayers[[i]] %*% c(
+        )))
+        yhat <- hlayers[[i]] %*% g(as.matrix(c(
           parlist$beta_param, parlist$beta_treatment, parlist$beta_treatmentinteractions, parlist$beta
-        ) + fe    
+        ))) + fe    
       } else {
-        yhat <- hlayers[[i]] %*% c(parlist$beta_param, parlist$beta_treatment, parlist$beta_treatmentinteractions, parlist$beta)
+        yhat <- hlayers[[i]] %*% g(as.matrix(c(parlist$beta_param, parlist$beta_treatment, parlist$beta_treatmentinteractions, parlist$beta)))
       }
-      mse <- mean((y-yhat)^2)
+      mse <- mean((y-h(yhat))^2)
       msevec <- append(msevec, mse)
-      loss <- mse + lam*sum(c(parlist$beta_param*parapen, 0*parlist$beta_treatment, parlist$beta, parlist$beta_treatmentinteractions, unlist(parlist[!grepl('beta', names(parlist))]))^2)
+      if (GPU == TRUE){
+        lowParVec <- unlist(sapply(parlist[sapply(parlist, class) == 'gmatrix'], h))#this works because only the lower-level weigts are on te GPU
+      } else {
+        lowParVec <- unlist(parlist[!grepl('beta', names(parlist))])
+      }
+      lossold <- loss
+      loss <- mse + lam*sum(c(parlist$beta_param*parapen, 0*parlist$beta_treatment, parlist$beta, parlist$beta_treatmentinteractions, lowParVec)^2)
       lossvec <- append(lossvec, loss)
       if (verbose == TRUE){
         writeLines(paste0(
@@ -300,10 +339,15 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
         plot(lossvec, type = 'l', main = 'all epochs')
         plot(lossvec[(1+(iter)*max(batchid)):length(lossvec)], type = 'l', ylab = 'loss', main = 'Current epoch')
       }
-    }
+    }#ends minibatch loop
     #Finished epoch.  Assess whether MSE has increased and revert if so
-    mse <- mean((y-yhat)^2)
-    loss <- mse + lam*sum(c(parlist$beta_param*parapen, 0*parlist$beta_treatment, parlist$beta, parlist$beta_treatmentinteractions, unlist(parlist[!grepl('beta', names(parlist))]))^2)
+    mse <- mean((y-h(yhat))^2)
+    if (GPU == TRUE){
+      lowParVec <- unlist(sapply(parlist[sapply(parlist, class) == 'gmatrix'], h))#this works because only the lower-level weigts are on te GPU
+    } else {
+      lowParVec <- unlist(parlist[!grepl('beta', names(parlist))])
+    }
+    loss <- mse + lam*sum(c(parlist$beta_param*parapen, 0*parlist$beta_treatment, parlist$beta, parlist$beta_treatmentinteractions, lowParVec)^2)
     #If loss increases...
     if (oldpar$loss < loss){
       parlist <- oldpar$parlist
@@ -335,6 +379,7 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
       }
     }
     iter = iter+1
+print(proc.time() - PT)
   } #close the while loop
 
 
