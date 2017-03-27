6e056ff39f20182533e0b7aa6b7131b48b1c4c3c
cranedroesch <andrewcd@gmail.com>
2017-03-27 17:25:15 -0400

    bugfix in response to problem with single parametric term

--- R/panelNNET.est.R
+++ R/panelNNET.est.R
@@ -358,10 +358,6 @@ getgr <- function(pl, skel = attr(pl, 'skeleton'), lam, parapen){
           }
           oldG2 <- lapply(G2, function(x){.9*x})
           G2 <- mapply('+', newG2, oldG2)
-hl <<- hlayers
-GG <<- G2
-GR <<- grads
-PL <<- parlist
           uB <- LR/sqrt(G2[[length(G2)]]+1e-10) *
             t(t(grads[[length(grads)]]) %*% hlayers[[length(hlayers)]][curBat,]) + 
             LR*as.matrix(2*lam*c(parlist$beta_param*parapen#penalty/weight decay...
