a73ecc95ec077427f4077ca3847bf54610c3112a
cranedroesch <andrewcd@gmail.com>
2017-08-21 15:47:19 -0400

    bugfix

--- R/panelNNET.est.R
+++ R/panelNNET.est.R
@@ -299,8 +299,8 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
         oldG2 <- lapply(G2, function(x){.9*x})
         G2 <- mapply('+', newG2, oldG2)
         uB <- LR/sqrt(G2[[length(G2)]]+1e-10) *
-          t(t(grads[[length(grads)]]) %*% hlayers[[length(hlayers)]][curBat,] + 
-          LR*2*lam*c(parlist$beta_param*parapen, parlist$beta))
+          t(as.matrix(t(grads[[length(grads)]]) %*% hlayers[[length(hlayers)]][curBat,] + 
+          LR*2*lam*c(parlist$beta_param*parapen, parlist$beta)))
         updates$beta_param <- uB[1:length(parlist$beta_param)]
         updates$beta <- uB[grepl('nodes', rownames(uB))]
         for(i in nlayers:1){
