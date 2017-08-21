37ed17a3eb894294dd8f512f0aa3cae0284efa3b
cranedroesch <andrewcd@gmail.com>
2017-08-21 15:05:49 -0400

    bugfix

--- R/panelNNET.est.R
+++ R/panelNNET.est.R
@@ -294,14 +294,14 @@ function(y, X, hidden_units, fe_var, maxit, lam, time_var, param, parapen, parli
       }
       #Calculate updates to parameters based on gradients and learning rates
       if (RMSprop == TRUE){
-        print("A")
+        print("E")
         newG2 <- foreach(i = 1:(length(hlayers)+1)) %do% {
           print(i)
           if (i == 1){D <- X[curBat,]} else {D <- hlayers[[i-1]][curBat,]}
           if (bias_hlayers == TRUE & i != length(hlayers)+1){D <- cbind(1, D)}
             .1*(t(D) %*% grads[[i]])^2
         }
-        print("B")
+        print("F")
         oldG2 <- lapply(G2, function(x){.9*x})
         G2 <- mapply('+', newG2, oldG2)
         uB <- LR/sqrt(G2[[length(G2)]]+1e-10) *
