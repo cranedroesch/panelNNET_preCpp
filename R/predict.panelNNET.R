4298bca7b3d264516f972f6a63b6d4faa051ea72
cranedroesch <andrewcd@gmail.com>
2017-08-26 13:45:01 -0400

    reworking of inference machinery to be compatible with sparse matrices and changes to OLStrick

--- R/predict.panelNNET.R
+++ R/predict.panelNNET.R
@@ -80,7 +80,8 @@ function(obj, newX = NULL, fe.newX = NULL, new.param = NULL, se.fit = FALSE
         vcnames[i] <- names(obj$vcs)[i]
         return(matrix(se))
       }
-      if (any(is.na(vcnames))){warning("One or more VCV has negative diagonals")}
+      print(head(semat))
+      # if (any(is.na(vcnames))){warning("One or more VCV has negative diagonals")}
       colnames(semat) <- vcnames[!is.na(vcnames)]
     }
     return(cbind(yhat, semat))
@@ -135,42 +136,6 @@ predfun <- function(plist, obj, newX = NULL, fe.newX = NULL, new.param = NULL,
   #otherwise...
   return(yhat)
 }
-
-
-# calc_hlayers <- function(parlist){
-#   hlayers <- vector('list', nlayers)
-#   for (i in 1:(nlayers + !is.null(convolutional))){
-#     if (i == 1){D <- X} else {D <- hlayers[[i-1]]}
-#     if (bias_hlayers == TRUE){D <- cbind(1, D)}
-#     # make sure that the time-invariant variables pass through the convolutional layer without being activated
-#     if (is.null(convolutional) | i > 1){
-#       hlayers[[i]] <- activ(D %*% parlist[[i]])        
-#     } else {
-#       HL <- D %*% parlist[[i]]
-#       HL[,1:(N_TV_layers * convolutional$Nconv)] <- activ(HL[,1:(N_TV_layers * convolutional$Nconv)])
-#       hlayers[[i]] <- HL
-#     }
-#   }
-#   colnames(hlayers[[i]]) <- paste0('nodes',1:ncol(hlayers[[i]]))
-#   if (!is.null(param)){#Add parametric terms to top layer
-#     hlayers[[i]] <- cbind(param, hlayers[[i]])
-#     colnames(hlayers[[i]])[1:ncol(param)] <- paste0('param',1:ncol(param))
-#   }
-#   if (is.null(fe_var)){#add intercept if no FEs
-#     hlayers[[i]] <- cbind(1, hlayers[[i]])
-#   }
-#   return(hlayers)
-# }
-
-
-
-
-
-
-
-
-
-
 
 
 
